98a7dd9623b110b877873934a5d0b10f39b081da
esam-gestionempresarial <direcciondegestionempresarial@esam.edu.bo>
2026-08-08 12:17:22 -0400

    fix: actualizacion de consulta

--- queries/consulta_banco_programas.sql
+++ queries/consulta_banco_programas.sql
@@ -5,7 +5,7 @@ SELECT
         WHEN c.nombre = 'Diplomado'   THEN CONCAT_WS('-', 'D', p.id)
         WHEN c.nombre = 'Especialidad' THEN CONCAT_WS('-', 'E', p.id)
         WHEN c.nombre = 'Maestría'    THEN CONCAT_WS('-', 'M', p.id)
-    END AS Id_Esam,
+    END AS Id_Portal,
     s.nombre AS sede,
     c.nombre AS tipo,
     i2.abreviatura AS convenio,
@@ -27,7 +27,7 @@ LEFT JOIN postgrados p2 ON p.idpostgrado = p2.id
 LEFT JOIN categorias c ON p2.idcategoria = c.id
 LEFT JOIN programa_universidad pu ON p.programa_universidad_id = pu.id
 LEFT JOIN estados_programas ep ON p.estado_programa_id = ep.id
-INNER JOIN inscripciones i ON i.idprograma = p.id
+LEFT JOIN inscripciones i ON i.idprograma = p.id
 LEFT JOIN modulo_programa_universidad mpu ON pu.id = mpu.programa_universidad_id
 INNER JOIN productionadminesamdb.instituciones i2 ON p.iduniversidad = i2.id
 INNER JOIN productionadminesamdb.sedes s ON p.idsede = s.id
@@ -44,6 +44,6 @@ LEFT JOIN (
     ) ranked_modulos
     WHERE rnk = 1
 ) ult_mod ON ult_mod.idprograma = p.id
-WHERE c.nombre REGEXP '^(Dip|Maes|Esp)'
-GROUP BY i2.abreviatura, s.nombre, c.nombre, pu.codigo, Id_Esam, p.nombre_compuesto, ep.parent_id, ep.nombre, p.estado_programa_id, p.fecha_inicio, p.fecha_fin, ult_mod.numero
+WHERE c.nombre REGEXP '^(Dip|Maes|Esp|Curso|Carrera|licenciatura|Téc)'
+GROUP BY i2.abreviatura, s.nombre, c.nombre, pu.codigo, Id_Portal, p.nombre_compuesto, ep.parent_id, ep.nombre, p.estado_programa_id, p.fecha_inicio, p.fecha_fin, ult_mod.numero
 ORDER BY pu.programa_unico_id ASC
