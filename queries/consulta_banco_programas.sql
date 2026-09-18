4744b48b0645eddf17536bc66966d8e21af19abb
esam-gestionempresarial <direcciondegestionempresarial@esam.edu.bo>
2026-09-18 14:58:06 -0400

    fix: actualizacion de consulta

--- queries/consulta_banco_programas.sql
+++ queries/consulta_banco_programas.sql
@@ -1,7 +1,7 @@
 SELECT
-    IFNULL(pu.programa_unico_id,"SIN ASIGNAR") AS cod_unico,
-    IFNULL(pu.codigo, "SIN ASIGNAR") AS cod_academico,
-    IFNULL(p.codigo, "SIN ASIGNAR") AS cod_contable,
+    IFNULL(pu.programa_unico_id,"sin registrar") AS cod_unico,
+    IFNULL(pu.codigo, "sin registrar") AS cod_academico,
+    IFNULL(p.codigo, "sin registrar") AS cod_contable,
     CASE
         WHEN c.nombre = 'Diplomado' THEN CONCAT_WS('-', 'D', p.id)
         WHEN c.nombre = 'Especialidad' THEN CONCAT_WS('-', 'E', p.id)
@@ -9,18 +9,30 @@ SELECT
     END AS id_portal,
     s.nombre AS sede,
     c.nombre AS tipo,
+    IFNULL(au.nombre,"sin registrar") AS area,
     i2.abreviatura AS convenio,
     p.nombre_compuesto AS programa,
+    IFNULL(pu.version,"sin registrar") AS version,
+    IFNULL(pu.tiene_ceub,"sin registrar") AS tiene_ceub,
+    IFNULL(pu.nomenclatura_hcu,"sin registrar") AS nomenclatura_hcu,
+    IFNULL(pu.horas_academicas_ceub,"sin registrar") AS horas_academicas_ceub,
+    IFNULL(pu.numero_creditos_ceub,"sin registrar") AS numero_creditos_ceub,
+    IFNULL(su.nombre,"sin registrar") AS sede_universidad,
     CASE
         WHEN ep.parent_id = 1 THEN "Comercialización"
         WHEN ep.parent_id = 2 THEN "Desarrollo"
         WHEN ep.parent_id = 3 THEN "Culminado"
+        ELSE "sin registrar"
     END AS fase,
-    ep.nombre AS estado,
-    p.fecha_inicio,
-    p.fecha_fin,
+    IFNULL(ep.nombre, "sin registrar") AS estado,
+    DATE(p.fecha_registro) AS fecha_registro_prog_portal,
+    p.fecha_inicio AS fecha_inicio_prog_portal,
+    p.fecha_fin AS fecha_fin_prog_portal,
+    pu.fecha_inicio AS fecha_inicio_prog_univ,
+    pu.fecha_fin AS fecha_fin_prog_univ,
+    IFNULL(pu.carga_horaria,"sin registrar") AS carga_horaria,
     COUNT(DISTINCT i.id) AS cant_registros,
-    COUNT(DISTINCT CASE WHEN i.estado_ins = 1 THEN i.id END) AS cant_inscritos,
+    COUNT(DISTINCT CASE WHEN ea.id = 3 OR ea.id = 9 THEN i.id END) AS cant_inscritos,
     IFNULL(ult_mod.numero, 0) AS fase_actual,
     IFNULL(COUNT(DISTINCT mpu.id), 0) AS cant_modulos
 FROM programas p
@@ -30,6 +42,9 @@ LEFT JOIN programa_universidad pu ON p.programa_universidad_id = pu.id
 LEFT JOIN estados_programas ep ON p.estado_programa_id = ep.id
 LEFT JOIN inscripciones i ON i.idprograma = p.id
 LEFT JOIN modulo_programa_universidad mpu ON pu.id = mpu.programa_universidad_id
+LEFT JOIN sede_universidad su ON pu.sede_universidad_id = su.id
+LEFT JOIN areas_universidades au ON pu.area_universidad_id = au.id
+LEFT JOIN estados_administrativos ea ON i.estado_administrativo_id = ea.id
 INNER JOIN productionadminesamdb.instituciones i2 ON p.iduniversidad = i2.id
 INNER JOIN productionadminesamdb.sedes s ON p.idsede = s.id
 /* ── Último módulo ── */
@@ -46,5 +61,5 @@ LEFT JOIN (
     WHERE rnk = 1
 ) ult_mod ON ult_mod.idprograma = p.id
 WHERE c.nombre REGEXP '^(Dip|Maes|Esp|Curso|Carrera|licenciatura|Téc)'
-GROUP BY i2.abreviatura, s.nombre, c.nombre, pu.codigo, p.codigo, Id_Portal, p.nombre_compuesto, ep.parent_id, ep.nombre, p.estado_programa_id, p.fecha_inicio, p.fecha_fin, ult_mod.numero
+GROUP BY i2.abreviatura, s.nombre, c.nombre, pu.codigo, p.codigo, Id_Portal, p.nombre_compuesto, ep.parent_id, ep.nombre, p.estado_programa_id, p.fecha_inicio, p.fecha_fin, ult_mod.numero, fecha_registro_prog_portal
 ORDER BY pu.programa_unico_id ASC
